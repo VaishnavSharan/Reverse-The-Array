3dc92c971eec91ab37386304f32626b7756dcd12
Vaishnav Sharan <98391200+VaishnavSharan@users.noreply.github.com>
2023-08-23 13:17:02 +0530

    Add files via upload

---
+++ Reverse The Array.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+void reverseArray(vector<int> &arr , int m)
+{
+	int s=m+1,e=arr.size()-1;
+	while(s<=e){
+		swap(arr[s],arr[e]);
+		s++;
+		e--;
+	}
+	
+	
+	
+}
